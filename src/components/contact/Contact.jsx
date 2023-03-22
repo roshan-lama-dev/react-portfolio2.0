c090cf4b4106353d04968d5d4782bfa06b0902f6
Havochalo <officialmail85@gmail.com>
2023-03-22 16:01:05 +1100

    Email Js integrated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,31 @@
 import { Button, TextField } from "@mui/material";
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.scss";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
 export const Contact = () => {
+  const form = useRef();
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_83s308j",
+        "template_978n7wp",
+        form.current,
+        "qLP0I-Ge24HExNiHT"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          if (result.text === "OK") {
+            toast.success("Email Sent");
+          }
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
   return (
     <div className="contact" id="contact">
@@ -12,7 +34,20 @@ export const Contact = () => {
       </div>
       <div className="right">
         <h2>Contact</h2>
-        <form onSubmit={handleOnSubmit}>
+        <form ref={form} onSubmit={handleOnSubmit}>
+          <TextField
+            style={{
+              marginTop: "10px",
+              backgroundColor: "white",
+              borderRadius: "15px",
+              marginBottom: "10px",
+            }}
+            id="outlined-basic"
+            label="Name"
+            variant="filled"
+            name="user_name"
+          />
+
           <TextField
             style={{
               marginTop: "10px",
@@ -23,8 +58,8 @@ export const Contact = () => {
             id="outlined-basic"
             label="Email"
             variant="filled"
+            name="user_email"
           />
-
           <TextField
             style={{
               marginTop: "10px",
@@ -35,11 +70,17 @@ export const Contact = () => {
             id="outlined-basic"
             label="Message"
             variant="filled"
+            name="message"
           />
 
           {/* <textarea placeholder="message"></textarea> */}
-          <Button type="submit" variant="contained" color="success">
-            Success
+          <Button
+            type="submit"
+            variant="contained"
+            color="success"
+            value="send"
+          >
+            Submit
           </Button>
           {/* <button type="submit">Submit</button> */}
         </form>
